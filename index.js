84ab65d7e2a480383c2582b9b442ccf48f16e185
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-07-10 09:58:46 +0200

    use execa.shell

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const execa = require('execa')
 
-execa('ls', ['-la'])
-.then(result => {
-  console.log(result.stdout)
-}, console.error)
+// const profilePath = '~/.bash_profile'
+// const cmd = `source #{profilePath} > /dev/null 2>&1; #{ls -la}`
+// const shellEnv = require("shell-env")
+
+execa.shell('ls -la')
+  .then(result => {
+    console.log(result.stdout)
+  }, console.error)
+
