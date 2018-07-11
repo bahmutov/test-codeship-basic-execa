2e71106142382347233ac3e8f6e12339beb6ed6e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-07-11 12:19:42 +0200

    ls without profile

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const electron = require('electron')
 console.log('started index')
-// const app = require('app')
-// const execa = require('execa')
+const execa = require('execa')
 
-// const listFiles = 'ls -la'
+const listFiles = 'ls -la'
 // const profilePath = '~/.bash_profile'
-// // const cmd = `source ${profilePath} > /dev/null 2>&1; ${listFiles}`
-// const cmd = listFiles
+// const cmd = `source ${profilePath} > /dev/null 2>&1; ${listFiles}`
+const cmd = listFiles
 
-// console.log('command: %s', cmd)
-// execa.shell(cmd)
-//   .then(result => {
-//     console.log(result.stdout)
-//   }, console.error)
+console.log('command: %s', cmd)
+execa.shell(cmd)
+  .then(result => {
+    console.log(result.stdout)
+  }, console.error)
 //   .then(() => {
 //     return execa.shell('ps -ax')
 //   })
 //   .then(result => {
 //     console.log(result.stdout)
 //   })
-//   .then(() => {
-//     console.log('quitting app')
-//     app.quit()
-//   })
-electron.app.quit()
+  .then(() => {
+    console.log('quitting app')
+    electron.app.quit()
+  })
