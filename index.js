b6fbb9e7988a63726c29c8023647f2b44dae7405
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-07-11 12:16:10 +0200

    quit electron app

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const execa = require('execa')
+const electron = require('electron')
+console.log('started index')
+// const app = require('app')
+// const execa = require('execa')
 
-const listFiles = 'ls -la'
-const profilePath = '~/.bash_profile'
-const cmd = `source ${profilePath} > /dev/null 2>&1; ${listFiles}`
+// const listFiles = 'ls -la'
+// const profilePath = '~/.bash_profile'
+// // const cmd = `source ${profilePath} > /dev/null 2>&1; ${listFiles}`
+// const cmd = listFiles
 
-console.log('command: %s', cmd)
-execa.shell(cmd)
-  .then(result => {
-    console.log(result.stdout)
-  }, console.error)
-  .then(() => {
-    return execa.shell('ps -ax')
-  })
-  .then(result => {
-    console.log(result.stdout)
-  })
+// console.log('command: %s', cmd)
+// execa.shell(cmd)
+//   .then(result => {
+//     console.log(result.stdout)
+//   }, console.error)
+//   .then(() => {
+//     return execa.shell('ps -ax')
+//   })
+//   .then(result => {
+//     console.log(result.stdout)
+//   })
+//   .then(() => {
+//     console.log('quitting app')
+//     app.quit()
+//   })
+electron.app.quit()
